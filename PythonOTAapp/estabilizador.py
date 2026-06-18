e58467fedd3207981454ede02fcb4b0694274be1
sobreiracaio <caiosobreira@gmail.com>
2026-06-18 15:33:25 -0300

    folder removed

--- PythonOTAapp/estabilizador.py
+++
@@ -1,6 +0,0 @@
-from vidstab import VidStab
-
-stabilizer = VidStab()
-stabilizer.stabilize(input_path='Video_Casa_REMOVER AUDIO.mp4',
-                     output_path='video_estavel.mp4',
-                     smoothing_window=30)
